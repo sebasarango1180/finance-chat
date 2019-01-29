3eb246480038a2e6a0a9c1f2282fb0cd42833da1
sebasarango1180 <sebasarango1180@gmail.com>
2019-01-29 18:42:16 -0500

    Defined view and URL for chat rooms

--- financechat/chatroom/views.py
+++ financechat/chatroom/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.utils.safestring import mark_safe
+import json
 
 
 def index(request):
+
     return render(request, 'chatroom/index.html', {})
+
+
+def room(request, room_name):
+
+    return render(request, 'chatroom/room.html', {
+        'room_name_json': mark_safe(json.dumps(room_name))
+    })
