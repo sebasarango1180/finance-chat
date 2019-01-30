6815c9fec979e79df21e5883704039a4ff0c4a74
sebasarango1180 <sebasarango1180@gmail.com>
2019-01-30 06:48:29 -0500

    Added /auth URL project-wise to allow logging in

--- financechat/financechat/urls.py
+++ financechat/financechat/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^chatroom/', include('chatroom.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^auth/', include('django.contrib.auth.urls')),
+    url('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
