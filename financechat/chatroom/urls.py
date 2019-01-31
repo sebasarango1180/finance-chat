224cc9fdf996ff78929b7b177e53b54c817d9642
sebasarango1180 <sebasarango1180@gmail.com>
2019-01-31 07:14:07 -0500

    Modified URL schema for chatroom app

--- financechat/chatroom/urls.py
+++ financechat/chatroom/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+    url('signup/', views.SignUp.as_view(), name='signup'),
+    url(r'^room/(?P<room_name>[^/]+)/$', views.room, name='room'),
 ]
