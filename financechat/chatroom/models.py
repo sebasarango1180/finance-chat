dfbc334872dcf4c5a069494bd66837ac4c94d9d1
sebasarango1180 <sebasarango1180@gmail.com>
2019-02-01 21:31:08 -0500

    Added model Manager instance to handle queries

--- financechat/chatroom/models.py
+++ financechat/chatroom/models.py
@@ -1,12 +1,34 @@
 import datetime
-#from django.db import models
 
 from djongo import models
+from django.contrib.auth.models import User
+
+MESSAGES_TO_DISPLAY = 50
+
+
+class _MessageManager(models.Manager):
+
+    def retrieve(self, room, limit=MESSAGES_TO_DISPLAY):
+
+        return super().filter(room=room)[:limit]
 
 
 class Message(models.Model):
 
     time_published = models.FloatField(default=datetime.datetime.utcnow().timestamp())
-    text = models.TextField()
-    user_id = models.ObjectIdField()
+    text = models.TextField(blank=False)
+    username = models.TextField(blank=False)
     room = models.TextField()
+
+    objects = _MessageManager()
+
+    class Meta:
+        ordering = ['-time_published']
+
+
+class Profile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    bio = models.TextField(max_length=400, blank=True)
+
