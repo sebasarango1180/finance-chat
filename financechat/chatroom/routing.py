324a3a13cc2fb3051674718e4e000776537372d5
sebasarango1180 <sebasarango1180@gmail.com>
2019-02-01 21:32:08 -0500

    Modified WebSocket route for chat rooms

--- financechat/chatroom/routing.py
+++ financechat/chatroom/routing.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import consumers
 
 websocket_urlpatterns = [
-    url(r'^ws/chatroom/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
+    url(r'^ws/chatroom/room/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
 ]
