66802930ef5a3be4d04f9cac65bdf1d3876cfa67
sebasarango1180 <sebasarango1180@gmail.com>
2019-03-15 11:50:37 -0500

    Removed register/ endpoint from general urls.py

--- financechat/financechat/urls.py
+++ financechat/financechat/urls.py
@@ -18,9 +18,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 
+from chatroom import views
+
 urlpatterns = [
     url(r'^chatroom/', include('chatroom.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^auth/', include('django.contrib.auth.urls')),
     url('', TemplateView.as_view(template_name='home.html'), name='home'),
+    #url(r'^register/', views.register, name='register')
 ]
