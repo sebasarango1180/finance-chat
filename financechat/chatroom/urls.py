4b969f799820c76f3e6221338571a8c7bcded75d
sebasarango1180 <sebasarango1180@gmail.com>
2019-03-15 11:48:42 -0500

    Added profile endpoint within chatroom app routing

--- financechat/chatroom/urls.py
+++ financechat/chatroom/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url('signup/', views.SignUp.as_view(), name='signup'),
+    url(r'^$', views.index, name='chatroom'),
     url(r'^room/(?P<room_name>[^/]+)/$', views.room, name='room'),
+    url(r'register/$', views.register, name='register'),
+    url(r'profile/$', views.show_profile, name='profile')
 ]
